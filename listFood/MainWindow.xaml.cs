f45dfb000e83cd9c658383a6bbb1d63532a73fb9
tongo0209 <tongo0209@gmail.com>
2020-05-28 12:43:05 +0700

    Push Menu

--- listFood/MainWindow.xaml.cs
+++ listFood/MainWindow.xaml.cs
@@ -45,12 +45,12 @@ namespace listFood
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            List<Food> listFoods = new List<Food>();
-            using Excel = Microsoft.Office.Interop.Excel;
-            Excel.Application xlApp = new Excel.Application();
-            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"sandbox_test.xlsx");
-            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
-            Excel.Range xlRange = xlWorksheet.UsedRange;
+            //List<Food> listFoods = new List<Food>();
+            //using Excel = Microsoft.Office.Interop.Excel;
+            //Excel.Application xlApp = new Excel.Application();
+            //Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"sandbox_test.xlsx");
+            //Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            //Excel.Range xlRange = xlWorksheet.UsedRange;
             
         }
     }
