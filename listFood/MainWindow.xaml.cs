aee96d10486afeee65f15a254107a5e9b784a235
tqxkn1112 <ntq20030807@gmail.com>
2020-06-14 20:28:01 +0700

    lại sai

--- listFood/MainWindow.xaml.cs
+++ listFood/MainWindow.xaml.cs
@@ -84,18 +84,25 @@ namespace listFood
             ListBox_Food.ItemsSource = listFood;
         }
 
-        private void Button_Home(object sender, RoutedEventArgs e)
+        private void StackPanel_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            Home hr = new Home();
-            hr.Show();
-            this.Hide();
+            if (e.ClickCount == 2)
+            {
+                MessageBox.Show("Đã ấn double click");
+            }
         }
+        //private void Button_Home(object sender, RoutedEventArgs e)
+        //{
+        //    Home hr = new Home();
+        //    hr.Show();
+        //    this.Hide();
+        //}
 
-        private void Button_Infomation(object sender, RoutedEventArgs e)
-        {
-            Infomation info = new Infomation();
-            info.Show();
-        }
+        //private void Button_Infomation(object sender, RoutedEventArgs e)
+        //{
+        //    Infomation info = new Infomation();
+        //    info.Show();
+        //}
 
         private void Button_List(object sender, RoutedEventArgs e)
         {
@@ -106,14 +113,14 @@ namespace listFood
         {
             DataContext = new ListFood();
         }
-        private void Button_Out(object sender, RoutedEventArgs e)
-        {
-            var DR = MessageBox.Show("Bạn có muốn thoát", "Cảnh báo", MessageBoxButton.YesNo, MessageBoxImage.Warning);
-            if(DR == MessageBoxResult.Yes )
-            {
-                Application.Current.Shutdown();
-            }    
-        }
+        //private void Button_Out(object sender, RoutedEventArgs e)
+        //{
+        //    var DR = MessageBox.Show("Bạn có muốn thoát", "Cảnh báo", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+        //    if (DR == MessageBoxResult.Yes)
+        //    {
+        //        Application.Current.Shutdown();
+        //    }
+        //}
     }
 }
 
