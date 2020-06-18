ea1fe58ea9f1b9faaffceccf4236b748fa7c1dc5
tongo0209 <tongo0209@gmail.com>
2020-06-18 09:39:38 +0700

    Edit

--- listFood/Dialog/Infomation.xaml.cs
+++ listFood/Dialog/Infomation.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,74 @@ using System.Windows.Shapes;
 
 namespace listFood.Dialog
 {
+    public class Info : INotifyPropertyChanged
+    {
+        private string _HUONGDAN;
+        public string HuongDan
+        {
+            get => _HUONGDAN; set
+            {
+                _HUONGDAN = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Huongdan"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+    public class Control : INotifyPropertyChanged
+    {
+        private string _Control;
+        public string CONTROL
+        {
+            get => _Control; set
+            {
+                _Control = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CONTROL"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
     /// <summary>
     /// Interaction logic for Infomation.xaml
     /// </summary>
     public partial class Infomation : UserControl
     {
+        
         public Infomation()
         {
             InitializeComponent();
+        }
+        BindingList<Info> _list;
+        BindingList<Control> _list2;
+        private void Info_Loaded(object sender, RoutedEventArgs e)
+        {
+            _list = new BindingList<Info>()
+            {
+                new Info() {HuongDan ="Trang chủ: ......................................................................."},
+                new Info() {HuongDan ="Trang chủ: ............"},
+                new Info() {HuongDan ="Trang chủ: ................................................"},
+                new Info() {HuongDan ="Trang chủ: ............................................................"},
+                new Info() {HuongDan ="Trang chủ: ........................................................"},
+                new Info() {HuongDan ="Trang chủ: ...................................................."},
+                new Info() {HuongDan ="Trang chủ: ............................................."},
+                new Info() {HuongDan ="Trang chủ: ...................................."},
+                new Info() {HuongDan ="Trang chủ: ......................................................."},
+            };
+            View_Info.ItemsSource = _list;
+            _list2 = new BindingList<Control>()
+            {
+                new Control() {CONTROL ="Trang chủ: ......................................................................."},
+                new Control() {CONTROL ="Trang chủ: ............"},
+                new Control() {CONTROL ="Trang chủ: ................................................"},
+                new Control() {CONTROL ="Trang chủ: ............................................................"},
+                new Control() {CONTROL ="Trang chủ: ........................................................"},
+                new Control() {CONTROL ="Trang chủ: ...................................................."},
+                new Control() {CONTROL ="Trang chủ: ............................................."},
+                new Control() {CONTROL ="Trang chủ: ...................................."},
+                new Control() {CONTROL ="Trang chủ: ......................................................."},
+            };
+            View_Control.ItemsSource = _list2;
         }
     }
 }
